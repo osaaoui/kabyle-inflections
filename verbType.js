d1b8d6a15785b7ebf9f7f0c9664e1016c05374b5
Omar Saaoui <osaaoui@gmail.com>
2021-03-16 10:37:24 -0400

    verbtype completed

--- verbType.js
+++ verbType.js
@@ -71,10 +71,13 @@ checkInflections: function(form){
     var lastTwoLettersAbs = form.slice(-2);
     var lastThreeLetters = form.slice(-3);
 
-
+    
     const {isFirst, firstInflection, verbType}=this.isFirstPersSing(form);
     const {isSecond, secondInflection, secondVerbType}=this.isSecondPersSing(form);
     const {isThird, thirdInflection, thirdVerbType}=this.isThirdPersSing(form);
+    const {isFirstPlur, firstPlurInflection, firstPlurVerbType}=this.isFirstPersPlur(form);
+    const {isSecondPlur, secondPlurInflection, secondPlurVerbType}=this.isSecondPersPlur(form);
+    const {isThirdPlur, ThirdPlurInflection, ThirdPlurVerbType}=this.isThirdPersPlur(form);
 
     if(isFirst){
         console.log("Form: 1st person singular");
@@ -101,24 +104,31 @@ checkInflections: function(form){
             thirdInflection,
             thirdVerbType
         }
+    }if(isFirstPlur){
+        console.log("Form: 1st person plural");
+        console.log("Type:" + firstPlurVerbType);
+        return {
+            isFirstPlur,
+            firstPlurInflection,
+            firstPlurVerbType
+        }
+    }if(isSecondPlur){
+        console.log("Form: 2nd person plural");
+        console.log("Type:" + secondPlurVerbType);
+        return {
+            isSecondPlur,
+            secondPlurInflection,
+            secondPlurVerbType
+        }
+    }if(isThirdPlur){
+        console.log("Form: 3rd person plural");
+        console.log("Type:" + ThirdPlurVerbType);
+        return {
+            isThirdPlur,
+            ThirdPlurInflection,
+            ThirdPlurVerbType
+        }
     }
-    
-    
-   /* 
-
-    else if (this.isThirdPersSing(form)){
-
-    }
-    else if (this.isFirstPersPlur(form)){
-
-    }
-    else if (this.isSecondtPersPlur(form)){
-
-    }
-    else if (this.isThirdPersPlur(form)){
-
-    }*/
-
     
 },
 
@@ -189,15 +199,18 @@ isThirdPersSing: function(form){
     var fourthChar = form.charAt(3);
     var fifththChar = form.charAt(4);
     var lastChar = form.charAt(form.length - 1);
+    var beforeLastChar = form.charAt(form.length - 2);
 
-    if ((firstChar === "i" ||firstChar === "y" || firstChar === "t") && (fourthChar !== fifththChar) && (lastChar !== "ḍ" && lastChar !== "a")) {// i/y/telmed
+    var formLength= form.length;
+
+    if ((firstChar === "i" ||firstChar === "y" || firstChar === "t") && (fourthChar !== fifththChar) && (lastChar !== "ḍ" && lastChar !== "a") && (beforeLastChar ==="e" && formLength <=6)) {// i/y/telmed
         isThird=true;
         thirdVerbType=_enums.LMED;
         
-    }else if((firstChar === "i" ||firstChar === "y" || firstChar === "t") && (fourthChar === fifththChar) && (lastChar !== "ḍ" && lastChar !== "a")){//tbeddel /ibeddel
+    }else if((firstChar === "i" ||firstChar === "y" || firstChar === "t") && (fourthChar === fifththChar) && (lastChar !== "ḍ" && lastChar !== "a") && (formLength <=7)){//tbeddel /ibeddel
         isThird=true;
         thirdVerbType=_enums.BEDDEL;
-    }else if((firstChar === "i" ||firstChar === "y" || firstChar === "t") && (fourthChar !== fifththChar) && lastChar ==="a"){ //i/y/techfa
+    }else if((firstChar === "i" ||firstChar === "y" || firstChar === "t") && (fourthChar !== fifththChar) && (lastChar ==="a") && (formLength <= 5)){ //i/y/tecfa
         isThird=true;
         thirdVerbType=_enums.CFU;
     }
@@ -210,9 +223,105 @@ isThirdPersSing: function(form){
     }
 },
 
+isFirstPersPlur: function(form){
+    var isFirstPlur= false;
+    var firstPlurVerbType;
+    var firstPlurInflection = _inflections.FIRSTPERSPLUR;
+    var firstChar = form.charAt(0);
+    var secondChar = form.charAt(1);
+    var fourthChar = form.charAt(3);
+    var fifththChar = form.charAt(4);
+    var beforeLastChar = form.charAt(form.length - 2);
+    var lastChar = form.charAt(form.length - 1);
 
+    if ((firstChar === "n" && secondChar === "e") && (fourthChar !== fifththChar) && (lastChar !== "a")) { // nelmed
+        isFirstPlur=true;
+        firstPlurVerbType=_enums.LMED;
+       
+    }else if((firstChar === "n" && beforeLastChar === "e") && (fourthChar === fifththChar)){//nbeddel
+        isFirstPlur=true;
+        firstPlurVerbType=_enums.BEDDEL;
+    }else if((firstChar ==="n" && secondChar === "e") && lastChar === "a"){ //necfa
+        isFirstPlur=true;
+        firstPlurVerbType=_enums.CFU;
+    }
+    return {
+    isFirstPlur,
+    firstPlurInflection,
+    firstPlurVerbType
+    }
+    
+},
 
+isSecondPersPlur:function(form){
+    var isSecondPlur=false;
+    var secondPlurVerbType;
+    var secondPlurInflection = _inflections.SECONDPERSPLUR;
 
+    var firstChar = form.charAt(0);
+    var secondChar = form.charAt(1);
+    var fourthChar = form.charAt(3);
+    var fifththChar = form.charAt(4);
+    var lastChars = form.slice(form.length - 3);
+    var lastTwoLetters = lastChars.slice(-2);
+
+    var formLength= form.length;
+
+    if ((firstChar === "t" && secondChar !== "e") && (lastTwoLetters == "em" || lastTwoLetters == "mt") && (fourthChar !== fifththChar)) {//tkecmem/t
+        isSecondPlur=true;
+        secondPlurVerbType=_enums.LMED;
+    }else if (firstChar === "t" &&(lastTwoLetters == "em" || lastTwoLetters == "mt") && (fourthChar === fifththChar)){//tbeddlem/t
+        isSecondPlur=true;
+        secondPlurVerbType=_enums.BEDDEL;
+    }else if ((firstChar === "t" && secondChar === "e") &&(lastTwoLetters == "am" || lastTwoLetters == "mt")  && (formLength >=6 )){//tecfam/t
+        isSecondPlur=true;
+        secondPlurVerbType=_enums.CFU;
+    }
+    return{
+        isSecondPlur,
+        secondPlurInflection,
+        secondPlurVerbType
+
+    }
+            
+},
+
+isThirdPersPlur: function(form){
+
+    var isThirdPlur=false;
+    var ThirdPlurVerbType;
+    var ThirdPlurInflection= _enums.THIRDPERSPLUR;
+
+    var thirdChar = form.charAt(2);
+    var fourthChar = form.charAt(3);
+    var lastChars = form.slice(form.length - 3);
+    var lastTwoLettersAbs = form.slice(-2);
+    var lastThreeLetters = form.slice(-3);
+    var formLength= form.length;
+
+    if ((lastTwoLettersAbs === "en" || lastThreeLetters == "ent") && (thirdChar !== fourthChar)) { // lemden/t
+        console.log("inside type lmed");
+        isThirdPlur=true;
+        ThirdPlurVerbType=_enums.LMED;
+        
+    }else if((lastTwoLettersAbs === "en" || lastThreeLetters == "ent") && (thirdChar === fourthChar)){ // beddlen/t
+        console.log("inside type beddel");
+        isThirdPlur=true;
+        ThirdPlurVerbType=_enums.BEDDEL;
+
+    }else if(lastTwoLettersAbs === "an" || lastThreeLetters == "ant" && (formLength <= 5)){ //cfan/t
+        console.log("inside type cfu");
+        isThirdPlur=true;
+        ThirdPlurVerbType=_enums.CFU;
+
+    }
+
+    return {
+        isThirdPlur,
+        ThirdPlurInflection,
+        ThirdPlurVerbType
+    }
+},
 
 };
 
